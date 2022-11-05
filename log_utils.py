243ea8acc46b82f0c626dc22fce89f2aa668a6df
jai-rajput97 <109294155+jai-rajput97@users.noreply.github.com>
2022-11-05 18:31:36 +0530

    Update log_utils.py

--- log_utils.py
+++ log_utils.py
@@ -1,6 +1,6 @@
 import logging
 def create_log_object(county_name, log_file_name_refex=''):
-    log_file_name = f"logs/debug_{county_name}.log"
+    log_file_name = f"multi_main/logs/debug_{county_name}.log"
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s",
